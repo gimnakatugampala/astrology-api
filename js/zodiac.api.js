5885bda25112ef5a86754cb3bf6c08f549b3ce2b
Gimna Katugampala <gimnakatugampala1@gmail.com>
2021-01-12 23:11:28 +0530

    init

--- js/zodiac.api.js
+++ js/zodiac.api.js
@@ -1,13 +1,13 @@
-const apiKey = 'eee7f4eee39ce09278c897dcd980dde0';
-const userId = '615417';
 
 //Fetch data Previous Day
 
 //Fetch data Today
 async function getToday(sign){
-    const res = await fetch(`http://horoscope-api.herokuapp.com/horoscope/today/${sign}`)
+    const res = await fetch(`https://horoscope-api.herokuapp.com/horoscope/today/${sign}`)
     const today = await res.json();
     return today;
 }
 
 //Fetch Day Next Day
+
+//fetch general data
