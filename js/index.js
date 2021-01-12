cbe020648ee3ef06b044912411e26325bbfe3294
Gimna Katugampala <gimnakatugampala1@gmail.com>
2021-01-13 01:14:55 +0530

    finall

--- js/index.js
+++ js/index.js
@@ -1,38 +1,73 @@
-    const previousBtn = document.getElementById('previous');
-    const todayBtn = document.getElementById('today');
-    const tommorrowBtn = document.getElementById('tommorrow');
-    const zodiacList = document.getElementById('zodiacList');
-    const generalInfo = document.getElementById('generalInfo');
+const previousBtn = document.getElementById('previous');
+const todayBtn = document.getElementById('today');
+const tommorrowBtn = document.getElementById('tommorrow');
+const zodiacList = document.getElementById('zodiacList');
+const results = document.getElementById('results');
 
-    //Get the value when clicked the today btn
-    todayBtn.addEventListener('click',function(e){
-        e.preventDefault();
-        getToday(zodiacList.value)
-        .then(data => console.log(data))
-        .catch(err => console.log(err))
+//Get the value when clicked the today btn
+todayBtn.addEventListener('click',function(e){
+    e.preventDefault();
+    getToday(zodiacList.value)
+    .then(data => {
+        results.innerHTML = `
+        <ul class="collection with-header">
+        <li class="collection-header"><h4>${data.compatibility}</h4></li>
+        <li class="collection-item">Current Date: ${data.current_date}</li>
+        <li class="collection-item">Lucky Number: ${data.lucky_number}</li>
+        <li class="collection-item">Lucky Time: ${data.lucky_time}</li>
+        <li class="collection-item">Color: ${data.color}</li>
+        <li class="collection-item">Date Range: ${data.date_range}</li>
+        <li class="collection-item">Mood: ${data.mood}</li>
+        <li class="collection-item">Description: ${data.description}</li>
+      </ul>  
+        `
     })
+    .catch(err => console.log(err))
+})
 
-     //Get the value when clicked the previous btn
-     previousBtn.addEventListener('click',function(e){
-        e.preventDefault();
-        console.log(zodiacList.value)
+ //Get the value when clicked the previous btn
+ previousBtn.addEventListener('click',function(e){
+    e.preventDefault();
+    getPrevious(zodiacList.value)
+    .then(data => {
+        results.innerHTML = `
+        <ul class="collection with-header">
+        <li class="collection-header"><h4>${data.compatibility}</h4></li>
+        <li class="collection-item">Current Date: ${data.current_date}</li>
+        <li class="collection-item">Lucky Number: ${data.lucky_number}</li>
+        <li class="collection-item">Lucky Time: ${data.lucky_time}</li>
+        <li class="collection-item">Color: ${data.color}</li>
+        <li class="collection-item">Date Range: ${data.date_range}</li>
+        <li class="collection-item">Mood: ${data.mood}</li>
+        <li class="collection-item">Description: ${data.description}</li>
+      </ul>  
+        `
     })
+    .catch(err => console.log(err))
+})
 
-     //Get the value when clicked the tommorrow btn
-     tommorrowBtn.addEventListener('click',function(e){
-        e.preventDefault();
-        console.log(zodiacList.value)
+ //Get the value when clicked the tommorrow btn
+ tommorrowBtn.addEventListener('click',function(e){
+    e.preventDefault();
+    getTomorrow(zodiacList.value)
+    .then(data => {
+        results.innerHTML = `
+        <ul class="collection with-header ">
+        <li class="collection-header"><h4>${data.compatibility}</h4></li>
+        <li class="collection-item">Current Date: ${data.current_date}</li>
+        <li class="collection-item">Lucky Number: ${data.lucky_number}</li>
+        <li class="collection-item">Lucky Time: ${data.lucky_time}</li>
+        <li class="collection-item">Color: ${data.color}</li>
+        <li class="collection-item">Date Range: ${data.date_range}</li>
+        <li class="collection-item">Mood: ${data.mood}</li>
+        <li class="collection-item">Description: ${data.description}</li>
+      </ul>  
+        `
     })
-
-     //Get Genral Info
-     generalInfo.addEventListener('click',function(e){
-        e.preventDefault();
-        console.log(zodiacList.value)
-    })
+    .catch(err => console.log(err))
+})
 
 
-
-
-    //init the select form
-    var elems = document.querySelectorAll('select');
-    M.FormSelect.init(elems, {});
+//init the select form
+var elems = document.querySelectorAll('select');
+M.FormSelect.init(elems, {});
--- js/zodiac.api.js
+++ js/zodiac.api.js
@@ -1,13 +1,30 @@
-
-//Fetch data Previous Day
+//Fetch Prevoius
+async function getPrevious(sign){
+    const head = {
+        method:'POST'
+    }
+    const res = await fetch(`https://aztro.sameerkumar.website/?sign=${sign}&day=yesterday`,head)
+    const yesterday = await res.json();
+    return yesterday;
+}
 
 //Fetch data Today
 async function getToday(sign){
-    const res = await fetch(`https://horoscope-api.herokuapp.com/horoscope/today/${sign}`)
+    const head = {
+        method:'POST'
+    }
+    const res = await fetch(`https://aztro.sameerkumar.website/?sign=${sign}&day=today`,head)
     const today = await res.json();
     return today;
 }
 
-//Fetch Day Next Day
+//Fetch Tommorrow
+async function getTomorrow(sign){
+    const head = {
+        method:'POST'
+    }
+    const res = await fetch(`https://aztro.sameerkumar.website/?sign=${sign}&day=tomorrow`,head)
+    const tomorrow  = await res.json();
+    return tomorrow;
+}
 
-//fetch general data
